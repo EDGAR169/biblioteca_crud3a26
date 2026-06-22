9844d743c92c1bdd642a544fd27bde816ced255d
EDGAR169 <hernandezedgargael23@gmail.com>
2026-06-22 10:33:46 -0600

    Mejoranddo la consulta de libros

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from dao.libro_dao import LibroDAO
 from models.libro import Libro
 
-def main(): 
+def  ver_libros():
+
+
     try:
         libro_dao = LibroDAO()#tIENE TODS LOS LIBRS DE DAO
 
@@ -25,6 +27,43 @@ def main():
     except Exception as e:
         print("Error:")
         print (e)
+
+
+def insertar_libro():
+    tiulo= imput("Escribe el titulo del nuevo libro:")
+    autor = int (input("Escribe el id del autor:"))
+    isbn = input("Escribe el isbn del nuevo libro:")
+    disponible = True
+    try:
+        libro_dao = LibroDAO()
+        id = libro_dao.obtener_ultimo_id()+ 1
+        libro = Libro(id,titulo,autor,isbm,disponible)
+        libro_dao.insert(libro)
+        print("Inserción realizada con éxito")
+    except Exeption as e:
+        print("Error al insertar nuevo libro")
+        print(e)
+
+def main():
+    print("===BIBLIOTECA UNIVERSAL===")
+    print("Menú de opciones")
+    print("1. Ver todos los libros")
+    print("2. insertar un nuevo libro")
+    print("3. Actualizar un libro disponible")
+    print("4. Elimina un libro disponible")   
+    opcion = int (input("Selecciona una opcion (1-4):"))
+
+    match opcion: 
+        case 1:
+            ver_libros()
+        case 2:
+            insertar_libros()
+        case 3: 
+            actualizar_libro()
+        case 4: 
+            eliminar_libro()
+
+    
 
 if __name__ == "__main__":
     main()            
--- dao/libro_dao.py
+++ dao/libro_dao.py
@@ -10,7 +10,10 @@ class LibroDAO:
         conexion = Conexion.obtener_conexion()#SEECT * FROM libro
         cursor = conexion.cursor()#Objeto para sql
 
-        cursor.execute("SELECT * FROM libro")
+        
+
+
+        cursor.execute("SELECT * FROM vista_libro")
         registros = cursor.fetchall()
 
         libros = []
@@ -31,9 +34,10 @@ class LibroDAO:
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
         sql = """
-        INSERT INTO libro( titulo,autor,isbn,disponible)
-        VALUES (%s,%s,%s,%s)
+        INSERT INTO libro(id,titulo,autor,isbn,disponible)
+        VALUES (%s,%s,%s,%s,%s)
         """
+
         #Los vaores que se pongam encursor seran iguales a los parametros que ingresamo
         cursor.execute(
             sql,
@@ -79,3 +83,18 @@ class LibroDAO:
         conexion.commit()
         cursor.close()
         conexion.close()
+
+def obtener_ultimo_id(self):
+    conexion = Conexion.obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT id FROM libro ORDER BY id DESC")
+    resultado = cursor.fetchone ()
+
+    cursor.close()
+    conexion.close()
+    
+
+    if resultado is None:
+        return 0
+    return resultado [0]
