6403022c0d8f3c530fa2557f346d3f416b2be086
EDGAR169 <hernandezedgargael23@gmail.com>
2026-07-08 12:02:41 -0600

    Creación de la ventana principal

--- ui/main_window.py
+++ ui/main_window.py
@@ -6,3 +6,81 @@ def main_window(page: ft.Page):
     page.window_height = 700
     page.padding = 0
     page.bgcolor = ft.Colors.BLUE_GREY_50
+    
+    #Ejemplo de widgwt Text
+    titulo = ft.Text(
+        "Sistema de Biblioteca Universitaria",
+        size = 24,
+        weight = ft.FontW.BOLD)
+
+    subtitulo = ft.Text (
+        "Selecciona una opcion de menú",
+        size = 16,
+        color = ft.Colors.BLUE_GREY_600
+        )
+    #Widget Container
+    contenido = ft.Container (
+        content = ft.Column(
+            controls = [
+                titulo,
+                subtitulo
+            ],
+            spacing = 10
+        ),
+        padding = 30,
+        expand = True
+    )
+
+    menu_lateral = ft.Container (
+        width = 220,
+        bgcolor = ft.Colors.BLUE_GREY_900,
+        padding = 20,
+        content = ft.Column(
+            controls = [
+                ft.Text(
+                    "Biblioteca",
+                    size = 22,
+                    weight =  ft.FontWeight.BOLD,
+                    color = ft.Colors.WHITE
+                ),
+                ft.Text(
+                    "Sistema de gestion",
+                    size = 12,
+                    color = ft.Colors.BLUE_GREY_100
+                ),
+                ft.Divider(color = ft.Colors.BLUE_GREY_700,),
+                ft.ElevatedButton(
+                    text = "Libros",
+                    icon = ft.Icons.BOOK,
+                    width  = 180,
+                ),
+                ft.ElevatedButton(
+                    text = "Usuarios",
+                    icon = ft.Icons.PERSON,
+                    width  = 180,
+                ),
+                ft.ElevatedButton(
+                    text = "Prestamo",
+                    icon = ft.Icons.SWAP_HORIZ,
+                    width  = 180,
+                ),
+                ft.ElevatedButton(
+                    text = "Devoluciones",
+                    icon = ft.Icons.KEYBOARD_RETURN,
+                    width  = 180,
+                ),
+                
+            ],
+            spacing = 15
+        )
+    )
+    #Agregar widghet a la pagina
+    layaut = ft.Row(
+        controls = [
+            menu_lateral,
+            contenido
+        ],
+        expand = True
+    )
+    page.add(layaud) #poner para ver la interfaz 
+
