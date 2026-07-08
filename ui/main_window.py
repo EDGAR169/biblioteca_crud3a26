6cd82e9f1b6f9401cde7e7c83750f76b5467875f
EDGAR169 <hernandezedgargael23@gmail.com>
2026-07-07 22:14:50 -0600

    Entorno virtual,estructura y dependencias

---
+++ ui/main_window.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+def main_window(page: ft.Page):
+    page.title = "Sistema de Biblioteca Universitaria"
+    page.window_width = 1100
+    page.window_height = 700
+    page.padding = 0
+    page.bgcolor = ft.Colors.BLUE_GREY_50
